3c8cb7d577f7ca096471ecbf0236e35763b0c647
N-S-AL-JABIR <n.s.al.jabir@gmail.com>
2025-04-14 16:56:20 +0600

    update

--- blog_project_m/blog_project_m/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from posts.models import Post
-def home(request):
-    post= Post.objects.all()
-    # print(post)
-    return render(request, "home.html", {"post": post})
--- blog_project_m/posts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("posts/", views.add_posts, name="add_posts"),
-    path("edit/<int:id>/", views.edit_post, name="edit_post"),
-    path("delete/<int:id>/", views.delete_post, name="delete_post"),
-]
