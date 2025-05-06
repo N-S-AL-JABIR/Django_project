49afaad9a7aaa1b9c5e7b5756c66ded0961a92ea
N-S-AL-JABIR <n.s.al.jabir@gmail.com>
2025-05-06 21:46:44 +0600

    remove error

--- Task_Management_System/TaskModel/urls.py
+++ Task_Management_System/TaskModel/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/", views.task, name="add_task"),
+    path("", views.task, name="add_task"),
     path("view_task/", views.view_task, name="view_task"),
     path("edit_task/<int:id>", views.edit_task, name="edit_task"),
     path("delete_task/<int:id>", views.delete_task, name="delete_task"),
